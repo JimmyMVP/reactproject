6abedc29ab8856bd696a17e8b3d2248faab031ad
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2016-06-25 16:52:35 +0200

    Added functionality with webpack

--- src/server.js
+++ src/server.js
@@ -3,13 +3,26 @@ express = require("express")
 react = require("express-react-views")
 server = express()
 
+webpackDev = require("webpack-dev-middleware")
+webpackHot = require("webpack-hot-middleware")
+webpackConfig = require("../webpack.dev.config")
+compiler = require("compiler")(webpackConfig)
 
+
+
+//Adding transpiler engine to the server
 server.engine("jsx", react.createEngine())
 server.set("view engine", "jsx")
 
+//Change views default directory to components
 server.set("views", __dirname + "/components")
 
 
+server.use(webpackDev(compiler, {
+    noInfo: true, publicPath: webpackConfig.output.publicPath
+}))
+
+server.use(webpackHot(compiler))
 
 server.get("/", (req, resp) => {
 
